17144b51205541f704ec9f7468f250734547c923
Thomas <thomasjnliu@gmail.com>
2020-03-22 00:00:14 -0400

    Add open/close door example

---
+++ Assets/Scripts/door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    [HideInInspector]
+    public SpriteRenderer Renderer;
+
+    public Sprite closeSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+        //closeSprite = Resources.Load<Sprite>("Sprites/doorClosed.png");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown(){
+        Debug.Log("Clicked");
+        Renderer.sprite = closeSprite;
+    }
+
+}
