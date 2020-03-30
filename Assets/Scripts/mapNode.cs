eb4089016b2c27d6b7ef342d85bd45a3d05f844e
Thomas <thomasjnliu@gmail.com>
2020-03-30 17:36:27 -0400

    Add map shadows

---
+++ Assets/Scripts/mapNode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapNode : MonoBehaviour
+{
+    public string location;
+
+    SpriteRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnMouseOver(){
+        Debug.Log("rollover");
+        renderer.color = new Color (0,0,0,0.0f);
+    }
+    
+}
