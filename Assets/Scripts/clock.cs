842980551dc8f39f581949c1e7430b8cf33e1cb8
Thomas <thomasjnliu@gmail.com>
2020-04-06 12:42:32 -0400

    Add clock displaying system time

---
+++ Assets/Scripts/clock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class clock : MonoBehaviour
+{
+    DateTime dt;
+    public TMP_Text clockText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dt = System.DateTime.Now;
+        Debug.Log(dt.ToString("hh:mm:ss"));   
+        clockText.text = dt.ToString("hh:mm:ss");
+    }
+}
