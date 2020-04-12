2a136da7b5d218c351b4a503aa3f5bcc2551cdc4
Thomas <thomasjnliu@gmail.com>
2020-04-12 14:32:21 -0400

    Add cookieManager, increment when cookie clicked

---
+++ Assets/Scripts/cookieManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cookieManager : MonoBehaviour
+{
+    public static cookieManager instance;
+    public GameObject cookie;
+
+    public int counter = 0;
+
+    void Awake(){
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            // A unique case where the Singleton exists but not in this scene
+            if (instance.gameObject.scene.name == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Increment(){
+        counter++;
+    }
+}
